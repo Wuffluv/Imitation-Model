87793f0abe6857ee6eec5c5d4835807d77a58bcc
Wuffluv <rodion.rycha@yandex.ru>
2024-11-16 14:45:09 +0900

    СМО есть

--- SMO/SMO/Form1.Designer.cs
+++ SMO/SMO/Form1.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(550, 285);
+            this.button1.Location = new System.Drawing.Point(754, 473);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 31);
             this.button1.TabIndex = 0;
@@ -51,23 +51,23 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(27, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(308, 192);
+            this.pictureBox1.Size = new System.Drawing.Size(489, 303);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(479, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(559, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(282, 187);
+            this.pictureBox2.Size = new System.Drawing.Size(523, 303);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(27, 262);
+            this.pictureBox3.Location = new System.Drawing.Point(27, 373);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(308, 176);
+            this.pictureBox3.Size = new System.Drawing.Size(489, 246);
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
@@ -75,7 +75,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1116, 663);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
--- SMO/SMO/Form1.cs
+++ SMO/SMO/Form1.cs
@@ -8,7 +8,6 @@ namespace SMO
 {
     public partial class Form1 : Form
     {
-        // Генератор случайных чисел для СМО
         private Random random = new Random();
 
         // Данные для графиков
@@ -17,12 +16,9 @@ namespace SMO
         private List<int> requestsRejected = new List<int>();
         private List<int> time = new List<int>();
 
-        // Параметры СМО
-        private const int MaxQueueLength = 10;
-        private const int TotalTicks = 1000;
-        private Queue<int> queue = new Queue<int>();
-        private int completedCount = 0;
-        private int rejectedCount = 0;
+        // Общие параметры
+        private const int TotalTicks = 1000;  // Количество тиков времени
+        private const int MaxQueueLength = 3; // Максимальная длина очереди
 
         public Form1()
         {
@@ -31,32 +27,52 @@ namespace SMO
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Сбрасываем данные
+            RunMultipleProcessors(); // СМО с несколькими процессорами
+        }
+
+        private void RunMultipleProcessors()
+        {
+            // Очередь заявок
+            Queue<int> queue = new Queue<int>();
+            int completedCount = 0;
+            int rejectedCount = 0;
+
+            // Процессоры (массив)
+            const int processorCount = 3;
+            int[] processors = new int[processorCount];
+
             ResetData();
 
-            // Основной цикл имитации
             for (int t = 0; t < TotalTicks; t++)
             {
                 time.Add(t);
 
-                // Генерация новой заявки
-                if (random.NextDouble() < 0.5) // Вероятность генерации заявки
+                // Генерация заявки
+                if (random.NextDouble() < 0.7) // Увеличена вероятность генерации заявок
                 {
                     if (queue.Count < MaxQueueLength)
                     {
-                        queue.Enqueue(t); // Добавляем заявку в очередь
+                        queue.Enqueue(t);
                     }
                     else
                     {
-                        rejectedCount++; // Заявка отклонена
+                        rejectedCount++;
                     }
                 }
 
-                // Обработка заявки
-                if (queue.Count > 0)
+                // Обработка заявок процессорами
+                for (int i = 0; i < processorCount; i++)
                 {
-                    queue.Dequeue();
-                    completedCount++;
+                    if (processors[i] == 0 && queue.Count > 0)
+                    {
+                        queue.Dequeue();
+                        processors[i] = random.Next(1, 5); // Случайное время обработки
+                        completedCount++;
+                    }
+                    else if (processors[i] > 0)
+                    {
+                        processors[i]--; // Уменьшение времени обработки
+                    }
                 }
 
                 // Сбор статистики
@@ -65,48 +81,80 @@ namespace SMO
                 requestsRejected.Add(rejectedCount);
             }
 
-            // Отрисовка графиков
-            DrawGraph(pictureBox1, time, requestsInQueue, "Заполненность очереди");
-            DrawGraph(pictureBox2, time, requestsCompleted, "Обработанные заявки");
-            DrawGraph(pictureBox3, time, requestsRejected, "Отклоненные заявки");
+            DrawGraphs();
         }
 
         private void ResetData()
         {
-            queue.Clear();
-            completedCount = 0;
-            rejectedCount = 0;
             requestsInQueue.Clear();
             requestsCompleted.Clear();
             requestsRejected.Clear();
             time.Clear();
         }
 
-        private void DrawGraph(PictureBox pictureBox, List<int> xData, List<int> yData, string title)
+        private void DrawGraphs()
+        {
+            DrawGraph(pictureBox1, time, requestsInQueue, "Заполненность очереди", "Время (тики)", "Число заявок");
+            DrawGraph(pictureBox2, time, requestsCompleted, "Обработанные заявки", "Время (тики)", "Число заявок");
+            DrawGraph(pictureBox3, time, requestsRejected, "Отклонённые заявки", "Время (тики)", "Число заявок");
+        }
+
+        private void DrawGraph(PictureBox pictureBox, List<int> xData, List<int> yData, string title, string xLabel, string yLabel)
         {
             Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
             using (Graphics g = Graphics.FromImage(bmp))
             {
                 g.Clear(Color.White);
 
-                // Отрисовка заголовка
-                g.DrawString(title, new Font("Arial", 10), Brushes.Black, new PointF(10, 10));
+                // Отрисовка заголовка графика
+                g.DrawString(title, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, new PointF(10, 10));
 
-                // Определение масштаба
-                float xScale = (float)pictureBox.Width / xData.Count;
-                float yScale = (float)pictureBox.Height / (yData.Max() + 1);
+                // Проверка на пустые данные
+                if (xData.Count == 0 || yData.Count == 0)
+                {
+                    g.DrawString("Нет данных", new Font("Arial", 10), Brushes.Red, new PointF(10, pictureBox.Height / 2));
+                    pictureBox.Image = bmp;
+                    return;
+                }
+
+                // Определение масштабов
+                float xScale = (float)(pictureBox.Width - 80) / xData.Count; // Учитываем отступы
+                float yScale = (float)(pictureBox.Height - 80) / (yData.Max() > 0 ? yData.Max() : 1);
 
                 // Отрисовка осей
-                g.DrawLine(Pens.Black, 0, pictureBox.Height - 20, pictureBox.Width, pictureBox.Height - 20);
-                g.DrawLine(Pens.Black, 20, 0, 20, pictureBox.Height);
+                g.DrawLine(Pens.Black, 60, pictureBox.Height - 40, pictureBox.Width - 20, pictureBox.Height - 40); // X-ось
+                g.DrawLine(Pens.Black, 60, 20, 60, pictureBox.Height - 40); // Y-ось
+
+                // Отрисовка делений на оси X
+                for (int i = 0; i <= xData.Count; i += xData.Count / 10)
+                {
+                    float xPos = 60 + i * xScale;
+                    g.DrawLine(Pens.Gray, xPos, pictureBox.Height - 40, xPos, 20); // Сетка
+                    g.DrawString((i).ToString(), new Font("Arial", 8), Brushes.Black, new PointF(xPos - 10, pictureBox.Height - 35));
+                }
+
+                // Отрисовка делений на оси Y
+                for (int i = 0; i <= yData.Max(); i += Math.Max(1, yData.Max() / 10))
+                {
+                    float yPos = pictureBox.Height - 40 - i * yScale;
+                    g.DrawLine(Pens.Gray, 60, yPos, pictureBox.Width - 20, yPos); // Сетка
+                    g.DrawString(i.ToString(), new Font("Arial", 8), Brushes.Black, new PointF(35, yPos - 7));
+                }
 
                 // Отрисовка данных
                 for (int i = 1; i < xData.Count; i++)
                 {
-                    g.DrawLine(Pens.Blue,
-                        20 + (i - 1) * xScale, pictureBox.Height - 20 - yData[i - 1] * yScale,
-                        20 + i * xScale, pictureBox.Height - 20 - yData[i] * yScale);
+                    float x1 = 60 + (i - 1) * xScale;
+                    float y1 = pictureBox.Height - 40 - yData[i - 1] * yScale;
+                    float x2 = 60 + i * xScale;
+                    float y2 = pictureBox.Height - 40 - yData[i] * yScale;
+
+                    g.DrawLine(new Pen(Color.Blue, 2), x1, y1, x2, y2); // Линия графика
                 }
+
+                // Подписи осей
+                g.DrawString(xLabel, new Font("Arial", 10), Brushes.Black, new PointF(pictureBox.Width / 2, pictureBox.Height - 20));
+                g.DrawString(yLabel, new Font("Arial", 10), Brushes.Black, new PointF(10, pictureBox.Height / 2), new StringFormat { FormatFlags = StringFormatFlags.DirectionVertical });
             }
 
             pictureBox.Image = bmp;
