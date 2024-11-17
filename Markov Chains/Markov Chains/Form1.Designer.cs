9f0448f812300c2f690a657ac323c4f97a48769c
Wuffluv <rodion.rycha@yandex.ru>
2024-11-17 22:30:21 +0900

    Цепи Маркова

---
+++ Markov Chains/Markov Chains/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Markov_Chains
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запуск моделирования";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(653, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(787, 418);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(33, 12);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(398, 373);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(582, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(402, 372);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(650, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Кол-во шагов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(784, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Кол-во состояний";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 488);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Markov Chains/Markov Chains/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Markov_Chains
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigureChart();
+        }
+
+        private void ConfigureChart()
+        {
+            // Настройка области графика
+            chart1.ChartAreas.Clear();
+            chart1.Series.Clear();
+            chart1.Titles.Clear();
+
+            // Добавление области графика
+            ChartArea chartArea = new ChartArea("MainArea");
+            chartArea.AxisX.Title = "Шаги (t)";
+            chartArea.AxisY.Title = "Состояние";
+            chartArea.AxisX.TitleFont = new Font("Arial", 12, FontStyle.Bold);
+            chartArea.AxisY.TitleFont = new Font("Arial", 12, FontStyle.Bold);
+            chartArea.AxisX.MajorGrid.LineColor = Color.LightGray;
+            chartArea.AxisY.MajorGrid.LineColor = Color.LightGray;
+            chartArea.AxisX.Interval = 1;
+            chart1.ChartAreas.Add(chartArea);
+
+            // Настройка серии
+            Series series = new Series("Состояния");
+            series.ChartType = SeriesChartType.Line;
+            series.Color = Color.Blue;
+            series.BorderWidth = 2;
+            series.MarkerStyle = MarkerStyle.Circle;
+            series.MarkerSize = 6;
+            series.MarkerColor = Color.Red;
+            chart1.Series.Add(series);
+
+            // Добавление заголовка
+            chart1.Titles.Add("График изменения состояний системы");
+            chart1.Titles[0].Font = new Font("Arial", 14, FontStyle.Bold);
+            chart1.Titles[0].Alignment = ContentAlignment.TopCenter;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Чтение данных
+            int t_max = int.Parse(textBox1.Text);
+            int n = int.Parse(textBox2.Text);
+            if (n <= 1)
+            {
+                MessageBox.Show("Количество состояний должно быть больше 1.");
+                return;
+            }
+
+            // Инициализация данных
+            Random random = new Random();
+            double[] p_vector = GenerateDistribution(n, random);
+            double[,] p_matrix = GenerateTransitionMatrix(n, random);
+            List<int> states = new List<int>();
+            int absorbingState = n - 1; // Поглощающее состояние
+
+            int totalTimeToAbsorption = 0;
+            int absorptionCount = 0;
+
+            for (int t = 0; t < t_max; t++)
+            {
+                // Генерация нового состояния
+                int currentState = RandomState(p_vector, random);
+                states.Add(currentState);
+
+                // Проверка на поглощение
+                if (currentState == absorbingState)
+                {
+                    totalTimeToAbsorption += t;
+                    absorptionCount++;
+                    break;
+                }
+
+                // Обновление распределения
+                p_vector = UpdateDistribution(p_vector, p_matrix);
+            }
+
+            // Обновление данных графика
+            chart1.Series["Состояния"].Points.Clear();
+            for (int i = 0; i < states.Count; i++)
+            {
+                chart1.Series["Состояния"].Points.AddXY(i, states[i]);
+            }
+
+            // Вывод результатов
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Среднее время до поглощения: " +
+                               (absorptionCount > 0 ? (totalTimeToAbsorption / absorptionCount).ToString() : "Не достигнуто"));
+            listBox1.Items.Add("Частота состояний:");
+            foreach (var group in states.GroupBy(x => x))
+            {
+                listBox1.Items.Add($"Состояние {group.Key}: {group.Count()}");
+            }
+        }
+
+        private double[] GenerateDistribution(int size, Random random)
+        {
+            double[] distribution = new double[size];
+            double sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                distribution[i] = random.NextDouble();
+                sum += distribution[i];
+            }
+            for (int i = 0; i < size; i++)
+            {
+                distribution[i] /= sum;
+            }
+            return distribution;
+        }
+
+        private double[,] GenerateTransitionMatrix(int size, Random random)
+        {
+            double[,] matrix = new double[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = random.NextDouble();
+                    sum += matrix[i, j];
+                }
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] /= sum;
+                }
+            }
+            return matrix;
+        }
+
+        private int RandomState(double[] probabilities, Random random)
+        {
+            double r = random.NextDouble();
+            double cumulative = 0;
+            for (int i = 0; i < probabilities.Length; i++)
+            {
+                cumulative += probabilities[i];
+                if (r < cumulative)
+                {
+                    return i;
+                }
+            }
+            return probabilities.Length - 1;
+        }
+
+        private double[] UpdateDistribution(double[] current, double[,] matrix)
+        {
+            double[] newDistribution = new double[current.Length];
+            for (int j = 0; j < current.Length; j++)
+            {
+                double sum = 0;
+                for (int i = 0; i < current.Length; i++)
+                {
+                    sum += current[i] * matrix[i, j];
+                }
+                newDistribution[j] = sum;
+            }
+            return newDistribution;
+        }
+    }
+}
